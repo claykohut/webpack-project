62148b31f00d4a902624d54af6acbd120b886aca
Clay Kohut <claykohut@MacBook-Pro-3.local>
2017-02-25 17:59:01 -0500

    added node server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+console.log('NODE_ENV ==', process.env.NODE_ENV)
+
+// Server routes...
+app.get('/hello', (req,res)=>{
+  res.send({hi: 'there'})
+})
+
+if(process.env.NODE_ENV !== 'production'){
+  const webpackMiddleware = require('webpack-dev-middleware')
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js')
+  app.use(webpackMiddleware(webpack(webpackConfig)))
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
+}
+
+app.listen(process.env.port || 3050, () => console.log('Listening!'))
